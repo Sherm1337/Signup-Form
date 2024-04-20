227719967c6c6d1b26b46719343b11fbcd1b22db
Sherm1337 <162771748+Sherm1337@users.noreply.github.com>
2024-04-20 01:15:21 -0500

    Fix pwd validator & center image citation

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let pwd = document.querySelector("#password").value;
-let confPwd = document.querySelector("#confirmPassword").value;
+let pwd = document.querySelector("#password");
+let confPwd = document.querySelector("#confirmPassword");
+let nomatchtxt = document.querySelector("#pwdnomatch");
+
+let confborder = document.querySelector(".text-input confpwd");
+let pwdborder = document.querySelector(".text-input pwd");
 
 function passConfirm() {
-    if (pwd !== confPwd) {
-        console.log("working");
+    if (pwd.value !== confPwd.value) {
+        nomatchtxt.style.visibility = "visible";
+        console.log("pwdconf all good");
     } else {
+        nomatchtxt.style.visibility = "hidden";
         console.log('not working');
     }
 }
