2f100ef70c50923ca78aa53baf3efdea729e82cd
Sherm1337 <162771748+Sherm1337@users.noreply.github.com>
2024-04-23 13:07:52 -0500

    Refactor JS and add label to required inputs

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ let pwd = document.querySelector("#password");
 let confPwd = document.querySelector("#confirmPassword");
 let nomatchtxt = document.querySelector("#pwdnomatch");
 
-let confborder = document.querySelector(".text-input confpwd");
-let pwdborder = document.querySelector(".text-input pwd");
 
 function passConfirm() {
     if (pwd.value !== confPwd.value) {
-        nomatchtxt.style.visibility = "visible";
-        console.log("pwdconf all good");
+        nomatchtxt.style.visibility = "visible";       
     } else {
         nomatchtxt.style.visibility = "hidden";
-        console.log('not working');
     }
 }
